3a3cd0bdc3fb63180533d63f5700ae33f9a3d87a
delaga <44586692+delaga@users.noreply.github.com>
2019-08-10 22:51:05 +0200

    PHP Vježbe04

--- polaznik26.edunova.hr/index.php
+++ polaznik26.edunova.hr/index.php
@@ -13,6 +13,29 @@
             echo "$i *13 is $val"."<br>";
             $i++;
         }
+        echo "<hr>";
+        for ($i=0; $i < 10; $i++) { 
+            $val=$i*10;
+            echo "$i *10 is $val"."<br>";
+        }
+        echo "<hr>";
+        for ($i=1; $i <=10 ; $i++) { 
+            echo "<br>";
+            for ($j=1; $j <=10 ; $j++) {
+                $tm=$i*$j; 
+               
+                echo "  $tm     "."-";
+            }
+        }
+        echo "<hr>";
+        $niz=array("Prvi","Drugi","Treci","Cetvrti","Peti");
+        foreach ($niz as $dumina) {
+            echo $dumina."<br>";
+        }
+        echo "<hr>";
+
+        
+
     ?>
 
 </body>
